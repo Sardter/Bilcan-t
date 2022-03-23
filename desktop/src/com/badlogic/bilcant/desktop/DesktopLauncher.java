fcdae8fbe9c0709958d27b5c502ce8bdfcf9810e
Sardter <sadra701@gmail.com>
2022-03-23 15:25:21 +0300

    first

---
+++ desktop/src/com/badlogic/bilcant/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badlogic.bilcant.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.bilcant.BilcantGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BilcantGame(), config);
+	}
+}
