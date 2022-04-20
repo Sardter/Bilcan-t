b7ee613e728fb3394d8ccbd1e46c886d66ee5506
Sardter <sadra701@gmail.com>
2022-04-20 16:43:19 +0300

    aaaa

--- core/src/com/badlogic/mygame/PlayerDetailScreen.java
+++ core/src/com/badlogic/mygame/PlayerDetailScreen.java
@@ -3,12 +3,15 @@ package com.badlogic.mygame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import org.w3c.dom.Text;
@@ -26,15 +29,19 @@ public class PlayerDetailScreen implements Screen {
     @Override
     public void show() {
         //game.changeScreen(BilcantGame.MENU);
-        Table table1 = new Table();
+        Table table1 = new Table(), table2 = new Table();
         table1.setFillParent(true);
+        table2.setFillParent(true);
+
         //table.setDebug(true);
         stage.addActor(table1);
+        stage.addActor(table2);
 
-        Skin skin = new Skin(Gdx.files.internal("level-plane/skin/level-plane-ui.json"));
+        Skin skin1 = new Skin(Gdx.files.internal("level-plane/skin/level-plane-ui.json"));
+        Skin skin2 = new Skin(Gdx.files.internal("pixthulhu/skin/pixthulhu-ui.json"));
 
-        TextButton backButton = new TextButton("Back", skin);
-        TextButton mainMenu = new TextButton("Main Menu", skin);
+        TextButton backButton = new TextButton("Back", skin1);
+        TextButton mainMenu = new TextButton("Main Menu", skin1);
         table1.add(backButton).pad(10);
         table1.add(mainMenu);
         table1.bottom();
@@ -50,6 +57,23 @@ public class PlayerDetailScreen implements Screen {
                 game.changeScreen(BilcantGame.MENU);
             }
         });
+
+        TextButton missions = new TextButton("Missions", skin2);
+        TextButton inventory = new TextButton("Inventory", skin2);
+        TextButton info = new TextButton("Info", skin2);
+
+        table2.add(missions).pad(10);
+        table2.row();
+        table2.add(inventory).pad(10);
+        table2.row();
+        table2.add(info).pad(10);
+
+        TextureRegionDrawable textureRegionDrawableBg =
+                new TextureRegionDrawable(new TextureRegion(
+                        new Texture(Gdx.files.internal("back2.jpeg"))));
+
+        table1.setBackground(textureRegionDrawableBg);
+
     }
 
     @Override
