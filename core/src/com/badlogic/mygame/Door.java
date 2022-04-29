f6ba5f2d966dbc8c7b5e16dd93408a6a1a8391bd
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2022-04-29 13:22:31 +0300

    bbora

---
+++ core/src/com/badlogic/mygame/Door.java
@@ -0,0 +1,28 @@
+/*
+package com.badlogic.mygame;
+
+import com.badlogic.mygame.models.*;
+
+public class Door extends GameObject{
+    final static int BOUNDRY_X = 800, BOUNDRY_Y = 480;
+    /*
+    public Door(int x, int y){
+        super(-2, "drop.png", 64, 64, x, y);
+    }
+    /*
+    @Override
+    public void interact(Object obj){
+        GameMap gm = (GameMap) obj;
+        GameMap newMap = new GameMap("menubackground.jpg", 800, 480, - BOUNDRY_X,  -BOUNDRY_Y);
+        obj = newMap;
+        System.out.println("1");
+    }
+    private void changeMap(){
+        GameMap newMap = new GameMap("menubackground.jpg", 800, 480, - BOUNDRY_X,  -BOUNDRY_Y);
+        obj = newMap;
+        System.out.println("1");
+    }
+
+
+}
+*/
